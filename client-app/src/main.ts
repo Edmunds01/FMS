41f1587934c70f9e0483b6a45aea1aefccf8991a
Edmunds Lukashevics <edmund090702@gmail.com>
2025-01-18 18:04:00 +0200

    Add Font Awesome integration and update index.html for icon support

--- client-app/src/main.ts
+++ client-app/src/main.ts
@@ -6,7 +6,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faXmark)
+
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
