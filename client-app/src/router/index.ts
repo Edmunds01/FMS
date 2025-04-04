4a64657deae5df1298002097a5a940d6890b4cd7
Edmunds Lukashevics <edmund090702@gmail.com>
2025-04-04 15:40:57 +0300

    Update index.ts

--- client-app/src/router/index.ts
+++ client-app/src/router/index.ts
@@ -49,25 +49,28 @@ const routes: RouteRecordRaw[] = [
   },
 ];
 
-// for (const route of routes) {
-//   if (route.name !== "login" && route.name !== "register") {
-//     route.meta = {
-//       requiresAuth: true,
-//     };
-//   }
-// }
+for (const route of routes) {
+  if (route.name !== "login" && route.name !== "register") {
+    route.meta = {
+      requiresAuth: true,
+    };
+  }
+}
 
 const router = createRouter({
   history: createWebHistory(),
   routes: routes,
 });
 
-// router.beforeEach((to, from, next) => {
-//   const token = localStorage.getItem("token") ?? "some-token-value";
-//   console.log("to", to);
-//   console.log("from", from);
-  
-//   next();
-// });
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem("token") ?? "some-token-value";
+  console.log("to", to);
+  console.log("from", from);
+  if (to.meta.requiresAuth && !token) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router;
