73e8d3534f8c201fa7e1847d6ddf2691dc745d62
Edmunds Lukashevics <edmund090702@gmail.com>
2025-04-04 12:23:10 +0300

    test

--- client-app/src/components/global/fa-icon.vue
+++ client-app/src/components/global/fa-icon.vue
@@ -21,42 +21,11 @@ export const icons = [
 </script>
 
 <script setup lang="ts">
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { computed } from "vue";
-
-export type IconName = (typeof icons)[number];
-type IconSize =
-  | "2xs"
-  | "xs"
-  | "sm"
-  | "lg"
-  | "xl"
-  | "2xl"
-  | "1x"
-  | "2x"
-  | "3x"
-  | "4x"
-  | "5x"
-  | "6x"
-  | "7x"
-  | "8x"
-  | "9x"
-  | "10x";
-
-const props = defineProps<{
-  iconName?: IconName | null | string;
-  size?: IconSize;
-}>();
-
-const defaultIcon = "money-bill-1";
-
-const resolvedIconName = computed(() => {
-  return icons.includes(props.iconName as IconName) ? props.iconName : defaultIcon;
-});
+export type IconName = typeof icons[number];
 </script>
 
 <template>
-  <font-awesome-icon v-if="iconName" :icon="'fa-solid fa-' + resolvedIconName" :size="size" />
+  <div>icon</div>
 </template>
 
 <style scoped></style>
