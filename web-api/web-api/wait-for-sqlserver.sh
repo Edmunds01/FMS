741203f8c51fa0fa32fdf106774d2d34c7894372
Edmunds Lukashevics <edmund090702@gmail.com>
2025-04-07 11:20:54 +0300

    Revert "wqe"
    This reverts commit c10e8c269153776e0ef20f069a8dfdd06633f55a.

--- web-api/web-api/wait-for-sqlserver.sh
+++ web-api/web-api/wait-for-sqlserver.sh
@@ -1,6 +1,6 @@
 echo "Waiting for SQL Server to start..."
-until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongPassword123 -Q "SELECT 1" > /dev/null 2>&1; do
-    echo "Try to select"
-    sleep 1
-done
+# until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongPassword123 -Q "SELECT 1" > /dev/null 2>&1; do
+#    echo "Try to select"
+#    sleep 1
+#done
 echo "SQL Server is up and running!"
