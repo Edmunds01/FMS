9e8353741df7ea5d91541240a78ca630c8561faf
Edmunds Lukashevics <edmund090702@gmail.com>
2025-04-07 11:21:10 +0300

    Revert "Test deploy"
    This reverts commit 905293ac889d598ad9ed1b3e17e4f10e3537009c.

--- web-api/web-api/bin/bash/wait-for-sqlserver.sh
+++
@@ -1,5 +0,0 @@
-echo "Waiting for SQL Server to start..."
-until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P YourStrongPassword123 -Q "SELECT 1" > /dev/null 2>&1; do
-    sleep 1
-done
-echo "SQL Server is up and running!"
